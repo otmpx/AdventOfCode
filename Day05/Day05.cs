4c3a2f700c385f3ba40be4b789a600f23b00c308
otmpx <owentmpx@gmail.com>
2022-12-05 17:53:03 +0800

    day5

--- Day05/Day05.cs
+++ Day05/Day05.cs
@@ -8,7 +8,7 @@ namespace AdventOfCock
 {
     internal class Day05 : IDay
     {
-        readonly List<List<char>> crates = new();
+        readonly List<List<char>> formation = new();
         readonly List<int> moveQuantity = new();
         readonly List<int> moveFrom = new();
         readonly List<int> moveTo = new();
@@ -17,23 +17,23 @@ namespace AdventOfCock
         {
             string[] init = File.ReadAllLines(inputA);
 
-            // Init list of crates length
+            // Init list of formation length
             for (int i = 0; i < init[0].Length; i += 4)
-                crates.Add(new List<char>());
+                formation.Add(new List<char>());
 
             // Add char from index of init[i]
-            for (int i = 0; i < init.Length - 1; i++)
-                for (int c = 0, v = 1; v < init[i].Length; c++, v += 4)
-                    crates[c].Add(init[i][v]);
+            for (int i = 0; i < init.Length - 1; i++) // Exclude the last line (that goes 0 1 2 3 4 5 6 7 8 9)
+                for (int c = 0, v = 1; v < init[i].Length; c++, v += 4) // index of v by char: [1] [5] [9] [14]...
+                    formation[c].Add(init[i][v]);
 
-            // Reverse and remove empty spaces from crates list
-            foreach (var item in crates)
+            // Reverse and remove empty spaces from formation list
+            foreach (var item in formation)
             {
                 item.Reverse();
                 item.RemoveAll(c => c == ' ');
             }
 
-            //crates = new List<List<char>>()
+            //formation = new List<List<char>>()
             //{
             //    new List<char>(){'J','H','P','M','S','F','N','V'},
             //    new List<char>(){'S','R','L','M','J','D','Q'},
@@ -49,6 +49,7 @@ namespace AdventOfCock
             string[] cmds = File.ReadAllLines(inputB);
             iterations = cmds.Length;
             string[] splitOptions = new string[] { "move ", " from ", " to " };
+
             for (int i = 0; i < cmds.Length; i++)
             {
                 // Split a string delimited by another string and return 3 non-empty elements: https://learn.microsoft.com/en-us/dotnet/api/system.string.split?view=net-7.0
@@ -62,11 +63,17 @@ namespace AdventOfCock
                 moveTo[i] -= 1;
             }
         }
-
         public void PartOne()
         {
+            // Doing this will still mutate the original list:
+            //List<List<char>> newFormation = new();
+            //newFormation.AddRange(formation);
+
+            // We need to do a deep copy for nested lists:
             List<List<char>> newFormation = new();
-            newFormation.AddRange(crates);
+            foreach (var item in formation)
+                newFormation.Add(new(item));
+
             for (int i = 0; i < iterations; i++)
             {
                 int takeQuantity = (int)MathF.Min(newFormation[moveFrom[i]].Count, moveQuantity[i]);
@@ -75,18 +82,35 @@ namespace AdventOfCock
                 newFormation[moveTo[i]].AddRange(take);
                 newFormation[moveFrom[i]].RemoveRange(newFormation[moveFrom[i]].Count - takeQuantity, takeQuantity);
 
-                // 0 1 2 3 -> count 4
-                // 0 1     -> take 2 (index 2-3)
+                // 0 1 2 3 -> Count 4
+                // 0 1     -> TakeLast 2 (index 2-3)
                 // remove index from 2 -> Count - takeQuantity 
             }
+
             string output = "";
             foreach (var item in newFormation)
                 output += item.Last();
             Console.WriteLine(output);
         }
-
         public void PartTwo()
         {
+            List<List<char>> newFormation = new();
+            foreach (var item in formation)
+                newFormation.Add(new(item));
+
+            for (int i = 0; i < iterations; i++)
+            {
+                int takeQuantity = (int)MathF.Min(newFormation[moveFrom[i]].Count, moveQuantity[i]);
+                if (takeQuantity == 0) continue;
+                var take = newFormation[moveFrom[i]].TakeLast(takeQuantity);
+                newFormation[moveTo[i]].AddRange(take);
+                newFormation[moveFrom[i]].RemoveRange(newFormation[moveFrom[i]].Count - takeQuantity, takeQuantity);
+            }
+
+            string output = "";
+            foreach (var item in newFormation)
+                output += item.Last();
+            Console.WriteLine(output);
         }
     }
 }

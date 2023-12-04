fdeda5604bce0c0c2dc9dfcc5f96a717fa542c17
otmpx <owentmpx@gmail.com>
2023-12-04 13:05:33 +0800

    a

--- AoC23/Day01/Day01.cs
+++ AoC23/Day01/Day01.cs
@@ -3,6 +3,10 @@ namespace AoC23
 {
     public class Day01 : IDay
     {
+        public Day01(string file)
+        {
+
+        }
         public void PartOne()
         {
             throw new NotImplementedException();
--- AoC23/obj/Debug/net8.0/AoC23.AssemblyInfo.cs
+++ AoC23/obj/Debug/net8.0/AoC23.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AoC23")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bfa2106b562e859dc5d20b36b846d07a40f4de9d")]
 [assembly: System.Reflection.AssemblyProductAttribute("AoC23")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AoC23")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

f2b7e47bcbddd338dccaa6266d11e9e4ea1c7a6d
otmpx <owentmpx@gmail.com>
2023-12-04 13:47:25 +0800

    day1

--- AoC23/Day01/Day01.cs
+++ AoC23/Day01/Day01.cs
@@ -3,18 +3,32 @@ namespace AoC23
 {
     public class Day01 : IDay
     {
+        List<string> input;
         public Day01(string file)
         {
-
+            input = new List<string>();
+            var lines = File.ReadAllLines(file);
+            foreach (var item in lines)
+                input.Add(item);
         }
         public void PartOne()
         {
-            throw new NotImplementedException();
+            List<int> sums = new List<int>();
+            foreach (var item in input)
+            {
+                List<int> numbers = new();
+                foreach (char c in item)
+                {
+                    //if (int.TryParse(c, out int num))
+                    //    numbers.Add(num);
+                }
+                sums.Add(numbers.First() + numbers.Last());
+            }
+            Console.Write(sums.Sum());
         }
 
         public void PartTwo()
         {
-            throw new NotImplementedException();
         }
     }
 }
---
+++ AoC23/bin/Debug/net8.0/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace AoC23
+{
+    internal class Program
+    {
+        static IDay? currentDay;
+        static void Main()
+        {
+            currentDay = new Day01("Day01/Input01.txt");
+            currentDay.PartOne();
+            currentDay.PartTwo();
+        }
+    }
+}
